40cb7f7ccc825c51b100162659045170c28ccfa7
Vanishree-kattimani <vanishreekattimani12@gmail.com>
2023-07-08 21:00:09 +0530

    Adding js file for dom html file

---
+++ gitFirstStart/dom.js
@@ -0,0 +1,27 @@
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document);
+//document.title = 123;
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent='Hello';
+//console.log(document.forms[0]);
+//console.log(document.links);
+//console.log(document.images);
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='Goodbye';
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML='<h3>Hello</h3>';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+
+var headerh2=document.getElementById('h2-title');
+headerh2.style.fontWeight="bold";
+headerh2.style.color="green";
