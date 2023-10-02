d23b433d52d14b1908981b0a35d5dbda7969a144
Vanishree-kattimani <vanishreekattimani12@gmail.com>
2023-10-02 19:11:44 +0530

    routing example

---
+++ nodeJs/formEx.js
@@ -0,0 +1,16 @@
+const http=require('http');
+
+
+  const server = http.createServer((req,res)=>{
+  const url =req.url;
+
+  if(url=='/') 
+  {
+    res.write('<html');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<bidy><form action="/message"method "POST"><input type="text"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+});
+  server.listen(3000);
