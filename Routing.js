32023ad7f0927810f86f7bcde374a60a6b96f5f4
Vanishree-kattimani <vanishreekattimani12@gmail.com>
2023-10-06 22:01:01 +0530

    express js task 2

--- Routing.js
+++ Routing.js
@@ -8,13 +8,23 @@
 //server.listen(3000);
 
 const express =require('express');
+const bodyParser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product', (req,res,next)=>{
+   // console.log('In the middleware!');
+   res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="product size"><button type="submit">Add product</button></form>')
+    
 });
-app.use((req,res,next)=>{
-    console.log('In another middleware!');
+app.post('/product',(req,res,next)=>{
+    console.log('req.body');
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    
     res.send('<h1>Hello from Express!</h1>');
 });
 
