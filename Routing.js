b0bf610f691c7141257206ef4b359c49f4133c6c
Vanishree-kattimani <vanishreekattimani12@gmail.com>
2023-10-06 20:05:00 +0530

    express js 1st task

--- Routing.js
+++ Routing.js
@@ -1,12 +1,21 @@
-const http =require('http')
+//const http =require('http')
 
-const route=require('./routes')
+//const route=require('./routes')
+//console.log(route.someText);
+//console.log('vanishree');
+//console.log("testing Nodemon");
+///const server = http.createServer(route.handler);
+//server.listen(3000);
 
+const express =require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In the middleware!');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
 
-console.log(route.someText);
-console.log('vanishree');
-console.log("testing Nodemon");
-
-
-const server = http.createServer(route.handler);
-server.listen(3000);
+app.listen(3000);
--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,9 +1,7 @@
 const http =require('http')
-
 const route=require('./routes')
-
-
 console.log(route.someText);
-
 const server = http.createServer(route.handler);
 server.listen(3000);
+
+
